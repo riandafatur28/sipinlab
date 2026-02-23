2ad17af0fc641c23664a97bdbc6bafed4850659e
Rianda <e41231605@student.polije.ac.id>
2026-02-23 15:24:53 +0700

    Pengiriman kode otp di email

---
+++ app/Mail/OtpMail.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OtpMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $otp;
+    public $expiresIn;
+    public $userName;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($otp, $expiresIn = 5, $userName = '')
+    {
+        $this->otp = $otp;
+        $this->expiresIn = $expiresIn;
+        $this->userName = $userName;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Kode OTP Reset Password - Polije',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.otp',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
